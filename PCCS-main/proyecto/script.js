e327a3dfeae6417216ca48ea3b6c4d999bcd180d
GeJonathan <88726982+GeJonathan@users.noreply.github.com>
2021-12-03 10:07:09 -0300

    Add files via upload

---
+++ PCCS-main/proyecto/script.js
@@ -0,0 +1,11 @@
+btnComponentes = document.getElementById('btnComponentes');
+
+btnComponentes.onclick = function componentes(){
+  window.open("../src/Guia/index.html", "_self");
+
+};
+
+btnarmado.onclick = function componentes(){
+  window.open("../src/Menu multinivel/index.html", "_self");
+
+};
